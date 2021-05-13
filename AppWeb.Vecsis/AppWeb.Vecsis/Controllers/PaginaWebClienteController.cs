816369d11495557bc470d3d06cd4c46ad098dc37
lixandro001 <gomezrinconlixandro@gmail.com>
2021-05-13 10:14:34 -0500

    avance lixandro

---
+++ AppWeb.Vecsis/AppWeb.Vecsis/Controllers/PaginaWebClienteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppWeb.Vecsis.Controllers
+{
+    public class PaginaWebClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
